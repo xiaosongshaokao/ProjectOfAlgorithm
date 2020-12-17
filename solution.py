0780b1f27c8bfba319ce32a228edd5cc09eccd4a
Songyicheng <songyicheng@sjtu.edu.cn>
2020-12-17 23:28:28 +0800

    破产版解类 1、写好了函数之间的逻辑关系 2、尚需计算穿墙次数和计算信号衰减的函数 3、background类需要的属性参考解类完成，目前需要一个rooms属性，包含每个房间的五点信息，此外包含一个wall属性，包含所有的墙的位置信息，以上两个属性可以直接存储，也可以写个函数计算得到

--- solution.py
+++ solution.py
@@ -5,3 +5,31 @@ class solution:
         if location is None:
             location = location_set
         self.location = location
+        self.fit_number = 0
+        self.emission_intensity = 100
+
+    def ThroughTheWall(self, AP, TestPoint, background):#计算从信号发射点到信号检测点穿墙次数
+        count = 0
+
+        return count
+
+    def reduction(self, times, distance):#计算从信号发射点到信号检测点的信号强度衰减
+        reduce = 1
+
+        return reduce
+
+    def fitness(self, background):#适应度函数，返回值为修改过的fit_number
+        results_in_background = []
+        for room in background.rooms:#计算每个房间的每个信号检测点的信号强度
+            results = []
+            TestPoint = room.points
+            for point in TestPoint:
+                result_at_the_point = 0
+                for AP in location_set:
+                    times = self.ThroughTheWall(AP, point, background)
+                    distance = ((AP[0] - point[0])**2 + (AP[1] - point[1]))**0.5
+                    reduction = self.reduction(times, distance)
+                    result_at_the_point += self.emission_intensity * reduction
+                results.append(result_at_the_point)
+            results_in_background.append(results)
+
