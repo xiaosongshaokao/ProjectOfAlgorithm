afca4193e3ae05b2c92da90bb2b16d7a25234920
Peacer68 <59168553+Peacer68@users.noreply.github.com>
2020-12-27 23:27:25 +0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
-#这个文件中需要写一个解类，此类需要包含一个解中所有的AP分布，并包含适应度函数
+# 这个文件中需要写一个解类，此类需要包含一个解中所有的AP分布，并包含适应度函数
 from env import *
 
 
-def pos2gene(pos):  #  将一个包含各AP位置的数组解码至基因型
+def pos2gene(pos):  # 将一个包含各AP位置的数组解码至基因型
     number = len(pos)
     gene = np.zeros(number * 13, dtype='int')  # 128 = 2^7, 64 = 2^6 各AP位置对应的基因型，先横坐标，后纵坐标
     for i in range(number):
@@ -19,16 +19,18 @@ def pos2gene(pos):  #  将一个包含各AP位置的数组解码至基因型
             cnt += 1
             gene[start_index + 13 - cnt] = row % 2
             row = row // 2
+        gene.tolist()
     return gene
 
+
 class solution:
-    def __init__(self, location=None):
+    def __init__(self, location):
         self.location = location
         self.fit_number = 0
         self.emission_intensity = 1
         self.gene = pos2gene(location)
 
-    def ThroughTheWall(self, AP, TestPoint, background):#计算从信号发射点到信号检测点穿墙次数
+    def ThroughTheWall(self, AP, TestPoint, background):  # 计算从信号发射点到信号检测点穿墙次数
         count = 0
         row = background.hwall
         column = background.vwall
@@ -71,34 +73,34 @@ class solution:
         return count
 
     # 计算从信号发射点到信号检测点的信号强度衰减
-    #根据公式，我舍去了常数部分，PLE暂设为0.5， 每穿过一堵墙的损耗暂设为10dB，均为经验值
+    # 根据公式，我舍去了常数部分，PLE暂设为0.5， 每穿过一堵墙的损耗暂设为10dB，均为经验值
     def reduction(self, times, distance):
         reduce = 1
         loss = (distance) ** 0.5 * 10 ** times
         reduce = reduce * loss
         return reduce
 
-    def fitness(self, background):#适应度函数，返回值为每个solution中的每个房间的所有监测点处信号强度
+    def fitness(self, background):  # 适应度函数，返回值为每个solution中的每个房间的所有监测点处信号强度
         results_in_background = []
-        for i in range(1, background.nroom + 1):#计算每个房间的每个信号检测点的信号强度
+        for i in range(1, background.nroom + 1):  # 计算每个房间的每个信号检测点的信号强度
             TestPoint = background.checkpoint[i]
             results = []
             All_point = []
-            for points in TestPoint:#points每个房间切分出的小矩形
+            for points in TestPoint:  # points每个房间切分出的小矩形
                 for j in points:
                     All_point.append(j)
             for point in All_point:
                 result_at_the_point = 0
                 for AP in self.location:
                     times = self.ThroughTheWall(AP, point, background)
-                    distance = ((AP[0] - point[0])**2 + (AP[1] - point[1])**2)**0.5
+                    distance = ((AP[0] - point[0]) ** 2 + (AP[1] - point[1]) ** 2) ** 0.5
                     reduction = self.reduction(times, distance)
                     result_at_the_point += self.emission_intensity * reduction
                 results.append(result_at_the_point)
             results_in_background.append(results)
         return results_in_background
 
-    def judgePopulation(self,results):#判断函数，用来决定特定种群这一组解的价值，后续可能会有调整
+    def judgePopulation(self, results):  # 判断函数，用来决定特定种群这一组解的价值，后续可能会有调整
 
         # def judgeAP(self, resultOfpoint):#加权函数
         #     value = resultOfpoint
